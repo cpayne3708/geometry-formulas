5a0d70dab01eecdc4ee707c15b28d96f7470d680
cpayne3708 <32746147+cpayne3708@users.noreply.github.com>
2018-08-23 12:37:24 -0400

    Add files via upload

--- Geometry.java
+++ Geometry.java
@@ -39,14 +39,50 @@ public class Geometry
     }
     
     public static void main(String[] args) 
+       {
+        System.out.println(distance(0,0,3,4));
+    }
+    
+    
+    
+    //my code
+    
+    public static double trapezoidArea(double b, double top, double h) 
     {
-        double ta1 = triangleArea(4.2, 8.1);
-        double ta2 = triangleArea(3.0, 123.157);
-        
-        System.out.println(ta1);
-        System.out.println(ta2);
-        
-        double ra1 = rectangleArea(5.2, 9.3);
-        System.out.println(ra1);
+        return ((b + top)/2) * h;
+    } 
+    
+    public static double rectPrismVolume(double l,double w,double h) 
+    {
+        return l * w * h;
+    } 
+    
+    public static double coneVolume(double r, double h) 
+    {
+        return (Math.PI * Math.pow(r,2)) * h / 3;
+    } 
+    
+    public static double rectPrismSurface(double l,double w, double h) 
+    {
+        return 2 * (l*w) + 2 * (l * h) + 2 * (w * h);
+    } 
+    
+    public static double sphereSurface(double r) 
+    {
+        return 4 * Math.PI * Math.pow(r,2);
+    } 
+    
+    public static double hypotenuse(double leg1,double leg2) 
+    {
+        return Math.sqrt(Math.pow(leg1,2) + Math.pow(leg2,2));
+    }
+    
+    public static double distance(double x1,double y1, double x2, double y2) 
+    {
+        return hypotenuse(x2-x1,y2-y1);
+    } 
+    
+    public static double slope(double x1,double y1, double x2, double y2) {
+        return (y2-y1) / (x2-x1);
     }
 }
